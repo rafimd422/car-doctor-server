80cac9526dbfeb18d0f301cf3fed6342abc651dd
rafimd422 <rafimf2222@gmail.com>
2023-10-27 15:17:45 +0600

    complated a delete api to delete single services from the bookings

--- index.js
+++ index.js
@@ -57,10 +57,17 @@ res.send(result)
 app.get('/booking', async (req,res) => {
   let query = {}
   if(req.query?.email){
-    query ={email: req.query.email}
+    query = {email: req.query.email}
   }
   const result = await bookingCollection.find(query).toArray()
   res.send(result)
+})
+
+app.delete('/booking/:id', async(req,res)=>{
+  const id = req.params.id;
+const query = {_id: new ObjectId(id)}
+const result = await bookingCollection.deleteOne(query)
+res.send(result)
 })
 
 
